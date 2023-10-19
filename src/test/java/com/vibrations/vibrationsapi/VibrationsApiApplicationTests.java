ca637f16b0475baa3b11368ae45bb60ff42895a2
David Clark <dfclark18@outlook.com>
2023-10-19 17:15:44 -0400

    added empty spring-boot project

---
+++ src/test/java/com/vibrations/vibrationsapi/VibrationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibrations.vibrationsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibrationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
