cf8ae902d8d88296c04a24b5823e4b2d0eedc7aa
David Clark <dfclark18@outlook.com>
2023-10-23 18:51:39 -0400

    Changed message

--- src/main/java/com/vibrations/vibrationsapi/controller/HelloController.java
+++ src/main/java/com/vibrations/vibrationsapi/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "S'all Good, Man";
+        return "S'all Good, Man (Better Call Saul!)";
     }
 }
