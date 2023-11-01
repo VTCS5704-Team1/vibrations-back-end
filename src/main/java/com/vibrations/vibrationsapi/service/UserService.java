3b90ba724ac125e1ea3a4b658132a8aa3fce0550
harditya007 <hardityathebest@gmail.com>
2023-11-01 10:56:57 -0400

    added user classes

---
+++ src/main/java/com/vibrations/vibrationsapi/service/UserService.java
@@ -0,0 +1,27 @@
+package com.vibrations.vibrationsapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        return user.orElse(null);
+    }
+}
