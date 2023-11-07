62c353eb3980e3487396ff00d2dc9e4b105d9a2e
harditya007 <hardityathebest@gmail.com>
2023-11-07 12:20:32 -0500

    Db integration

--- src/main/java/com/vibrations/vibrationsapi/model/User.java
+++ src/main/java/com/vibrations/vibrationsapi/model/User.java
@@ -13,18 +13,31 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "profile_picture_link")
     private String profilePictureLink;
+    @Column(name = "email")
     private String email;
+    @Column(name = "phone_number")
     private Long phoneNumber;
+    @Column(name = "password")
     private String password;
+    @Column(name = "fav_song")
     private List<String> favSong;
+    @Column(name = "fav_artist")
     private List<String> favArtist;
+    @Column(name = "fav_genre")
     private List<String> favGenre;
+    @Column(name = "score")
     private double score;
+    @Column(name = "latitude")
     private double latitude;
+    @Column(name = "longitude")
     private double longitude;
     //@ManyToMany
     //private List<User> matches;
