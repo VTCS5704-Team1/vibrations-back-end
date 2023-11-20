165221734e9917416c377c63af214c6f4d165009
David Clark <dfclark18@outlook.com>
2023-11-20 18:49:48 -0500

    Deleted outdated class

--- src/main/java/com/vibrations/vibrationsapi/VibrationsApiApplication.java
+++ src/main/java/com/vibrations/vibrationsapi/VibrationsApiApplication.java
@@ -1,12 +1,9 @@
 package com.vibrations.vibrationsapi;
 
-import com.vibrations.vibrationsapi.security.JwtDecoderConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(JwtDecoderConfiguration.class)
 public class VibrationsApiApplication {
 
 	public static void main(String[] args) {
