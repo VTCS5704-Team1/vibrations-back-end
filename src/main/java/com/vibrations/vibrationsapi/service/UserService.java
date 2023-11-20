7f6540c6519db953868d38dcd26a0584c87ec376
David Clark <dfclark18@outlook.com>
2023-11-20 14:32:58 -0500

    Added Comments for UserService methods

--- src/main/java/com/vibrations/vibrationsapi/service/UserService.java
+++ src/main/java/com/vibrations/vibrationsapi/service/UserService.java
@@ -1,12 +1,20 @@
 package com.vibrations.vibrationsapi.service;
 
-import com.vibrations.vibrationsapi.dto.SignInRequestDto;
-import com.vibrations.vibrationsapi.dto.SignInResponseDto;
-import com.vibrations.vibrationsapi.dto.SignUpRequestDto;
-import com.vibrations.vibrationsapi.dto.SignUpResponseDto;
+import com.vibrations.vibrationsapi.dto.*;
+import org.springframework.http.ResponseEntity;
 
 public interface UserService {
+    /**
+     * Method for registering an account using AWS Cognito
+     * @param signUpRequest -- POST RequestBody for signing up
+     * @return Response to signup request
+     */
     SignUpResponseDto signUp(SignUpRequestDto signUpRequest);
 
+    /**
+     * Method for signing in AWS Cognito User Pool
+     * @param signInRequest -- POST RequestBody for signing in
+     * @return Response to sign in request
+     */
     SignInResponseDto signIn(SignInRequestDto signInRequest);
 }
