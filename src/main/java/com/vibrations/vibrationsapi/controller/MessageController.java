fdf6b9fbd275c3153b4fb6290cc9f5fd40287bfc
harditya007 <hardityathebest@gmail.com>
2023-11-26 15:50:39 -0500

    Added register and image save

--- src/main/java/com/vibrations/vibrationsapi/controller/MessageController.java
+++ src/main/java/com/vibrations/vibrationsapi/controller/MessageController.java
@@ -1,51 +1,51 @@
-package com.vibrations.vibrationsapi.controller;
-
-import com.vibrations.vibrationsapi.model.Message;
-import com.vibrations.vibrationsapi.model.User;
-import com.vibrations.vibrationsapi.service.MessageService;
-import com.vibrations.vibrationsapi.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/api/messages")
-public class MessageController {
-    @Autowired
-    private MessageService messageService;
-    @Autowired
-    private UserService userService;
-
-    @PostMapping
-    public ResponseEntity<Message> sendMessage(
-            @RequestParam String senderEmail,
-            @RequestParam String receiverEmail,
-            @RequestParam String content) {
-
-        Optional<User> sender = userService.findUserByEmail(senderEmail);
-        Optional<User> receiver = userService.findUserByEmail(receiverEmail);
-
-        if (sender.isPresent() && receiver.isPresent()) {
-            Message message = messageService.sendMessage(sender.get(), receiver.get(), content);
-            return new ResponseEntity<>(message, HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/{email}")
-    public ResponseEntity<List<Message>> getMessages(@PathVariable String email) {
-        Optional<User> user = userService.findUserByEmail(email);
-
-        if (user.isPresent()) {
-            List<Message> messages = messageService.getMessages(user.get());
-            return new ResponseEntity<>(messages, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-}
+//package com.vibrations.vibrationsapi.controller;
+//
+//import com.vibrations.vibrationsapi.model.Message;
+//import com.vibrations.vibrationsapi.model.User;
+//import com.vibrations.vibrationsapi.service.MessageService;
+//import com.vibrations.vibrationsapi.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@RestController
+//@RequestMapping("/api/messages")
+//public class MessageController {
+//    @Autowired
+//    private MessageService messageService;
+//    @Autowired
+//    private UserService userService;
+//
+//    @PostMapping
+//    public ResponseEntity<Message> sendMessage(
+//            @RequestParam String senderEmail,
+//            @RequestParam String receiverEmail,
+//            @RequestParam String content) {
+//
+//        Optional<User> sender = userService.findUserByEmail(senderEmail);
+//        Optional<User> receiver = userService.findUserByEmail(receiverEmail);
+//
+//        if (sender.isPresent() && receiver.isPresent()) {
+//            Message message = messageService.sendMessage(sender.get(), receiver.get(), content);
+//            return new ResponseEntity<>(message, HttpStatus.CREATED);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @GetMapping("/{email}")
+//    public ResponseEntity<List<Message>> getMessages(@PathVariable String email) {
+//        Optional<User> user = userService.findUserByEmail(email);
+//
+//        if (user.isPresent()) {
+//            List<Message> messages = messageService.getMessages(user.get());
+//            return new ResponseEntity<>(messages, HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//}
--- src/main/java/com/vibrations/vibrationsapi/service/impl/MessageServiceImpl.java
+++ src/main/java/com/vibrations/vibrationsapi/service/impl/MessageServiceImpl.java
@@ -1,32 +1,32 @@
-package com.vibrations.vibrationsapi.service.impl;
-
-import com.vibrations.vibrationsapi.model.Message;
-import com.vibrations.vibrationsapi.model.User;
-import com.vibrations.vibrationsapi.repository.MessageRepository;
-import com.vibrations.vibrationsapi.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Service
-public class MessageServiceImpl implements MessageService {
-    @Autowired
-    private MessageRepository messageRepository;
-
-    @Override
-    public Message sendMessage(User sender, User receiver, String content) {
-        Message message = new Message();
-        message.setSenderEmail(sender);
-        message.setReceiverEmail(receiver);
-        message.setContent(content);
-        message.setTimestamp(LocalDateTime.now());
-        return messageRepository.save(message);
-    }
-
-    @Override
-    public List<Message> getMessages(User user) {
-        return messageRepository.findBySenderOrReceiver(user, user);
-    }
-}
+//package com.vibrations.vibrationsapi.service.impl;
+//
+//import com.vibrations.vibrationsapi.model.Message;
+//import com.vibrations.vibrationsapi.model.User;
+//import com.vibrations.vibrationsapi.repository.MessageRepository;
+//import com.vibrations.vibrationsapi.service.MessageService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@Service
+//public class MessageServiceImpl implements MessageService {
+//    @Autowired
+//    private MessageRepository messageRepository;
+//
+//    @Override
+//    public Message sendMessage(User sender, User receiver, String content) {
+//        Message message = new Message();
+//        message.setSenderEmail(sender);
+//        message.setReceiverEmail(receiver);
+//        message.setContent(content);
+//        message.setTimestamp(LocalDateTime.now());
+//        return messageRepository.save(message);
+//    }
+//
+//    @Override
+//    public List<Message> getMessages(User user) {
+//        return messageRepository.findBySenderOrReceiver(user, user);
+//    }
+//}
